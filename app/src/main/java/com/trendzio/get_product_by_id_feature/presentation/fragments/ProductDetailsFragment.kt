806305258bf841c720c7d099c3f3fc0518f7b348
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-02-17 18:39:57 +0530

    [Code Changes]: Handled the error by showing the toast message to the user.

--- app/src/main/java/com/trendzio/get_product_by_id_feature/presentation/fragments/ProductDetailsFragment.kt
+++ app/src/main/java/com/trendzio/get_product_by_id_feature/presentation/fragments/ProductDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import coil.load
@@ -57,7 +58,9 @@ class ProductDetailsFragment : Fragment() {
 
                 }
 
-                is NetworkResult.Error -> {}
+                is NetworkResult.Error -> {
+                    Toast.makeText(requireContext(), it.data?.errorBody().toString(), Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
--- app/src/main/java/com/trendzio/get_products_list_feature/presentation/fragments/ProductListFragment.kt
+++ app/src/main/java/com/trendzio/get_products_list_feature/presentation/fragments/ProductListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -63,7 +64,7 @@ class ProductListFragment : Fragment() {
                 }
 
                 is NetworkResult.Error -> {
-
+                    Toast.makeText(requireContext(), it.data?.errorBody().toString(), Toast.LENGTH_SHORT).show()
                 }
             }
         }
